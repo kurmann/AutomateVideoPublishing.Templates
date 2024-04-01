58500cc30c96e6d2e6ff374a3f1114afaa831a22
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-04-01 14:26:51 +0200

    remove unnecessary using

--- templates/scimodule/Application/Program.cs
+++ templates/scimodule/Application/Program.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Kurmann.Videoschnitt.ServiceCollectionIntegratedModule;
 
 namespace Kurmann.Videoschnitt.ServiceCollectionIntegratedModule.Application;
 
