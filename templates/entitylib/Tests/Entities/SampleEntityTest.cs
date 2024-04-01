7cba760838fa0d7edeae074bef56638a66e1f79a
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-04-01 14:04:59 +0200

    do not include test results

---
+++ templates/entitylib/Tests/Entities/SampleEntityTest.cs
@@ -0,0 +1,21 @@
+using Kurmann.Videoschnitt.ServiceCollectionIntegratedModule.Entities;
+
+namespace Kurmann.Videoschnitt.ServiceCollectionIntegratedModule.Tests.Entities;
+
+[TestClass]
+public class SampleEntityTest
+{
+    [TestMethod]
+    public void Create_WithEmptySampleParameter_ReturnsFailure()
+    {
+        // Arrange
+        var sampleParameter = string.Empty;
+
+        // Act
+        var result = SampleEntity.Create(sampleParameter);
+
+        // Assert
+        Assert.IsTrue(result.IsFailure);
+        Assert.AreEqual("Sample parameter cannot be empty", result.Error);
+    }
+}
